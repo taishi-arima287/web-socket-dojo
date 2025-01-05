c607380d7135fcbfc5899ffbfb6e351e72d3c4aa
taishi-arima287 <taishi@arima-system.com>
2025-01-05 14:28:51 +0900

    fix nextの設定ファイルを統合

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    forceSwcTransforms: true,
+  },
 };
 
 export default nextConfig;
